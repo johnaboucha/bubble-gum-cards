6cc90efd09cc74c17d5a1fd0aa633a9b0f9c5fba
John Boucha <johnaboucha@users.noreply.github.com>
2022-10-13 20:26:35 -0500

    routes done

--- helpers.py
+++ helpers.py
@@ -7,16 +7,22 @@ def get_categories():
 	data_json = json.loads(response.read())
 	return data_json
 
-def get_cards(skip=0, top=9):
-	url = "https://2wf0rg.deta.dev/api/cards/?skip="+str(skip)+"&top="+str(top)
+def get_cards(skip=0, top=9, category=""):
+	if category != "":
+		url = "https://2wf0rg.deta.dev/api/cards/?category="+category+"&skip="+str(skip)+"&top="+str(top)
+	else:
+		url = "https://2wf0rg.deta.dev/api/cards/?skip="+str(skip)+"&top="+str(top)
+
 	response = urlopen(url)
 	data_json = json.loads(response.read())
 	return data_json
 
-def get_next(to_skip=0):
-	url = "https://2wf0rg.deta.dev/api/cards/?skip="+str(to_skip)+"&top=1"
+def has_next(url="", to_skip=0):
+	#url = "https://2wf0rg.deta.dev/api/cards/?skip="+str(to_skip)+"&top=1"
 	response = urlopen(url)
 	data_json = json.loads(response.read())
+	print("length: " + str(len(data_json)) + " : "+str(to_skip))
+	print(data_json)
 	if len(data_json) > 0:
 		return True
 	return False
--- main.py
+++ main.py
@@ -1,31 +1,49 @@
 from webbrowser import get
-from flask import Flask, render_template, url_for
-from helpers import get_categories, get_cards, get_next
+from flask import Flask, render_template, url_for, redirect
+from helpers import get_categories, get_cards, has_next
 
 app = Flask(__name__)
 
 @app.route('/')
 def root():
-	categories = get_categories()
-	cards = get_cards()
-	has_next = get_next(10)
-
-	return render_template('home.html', categories=categories, cards=cards, has_next=has_next, has_prev=False, next_page=2)
+	return redirect('/page/1')
 
 @app.route('/page/<int:page>')
 def page(page):
 	skip = (page - 1) * 9
 	categories = get_categories()
 	cards = get_cards(skip=skip)
-	has_next = get_next(skip+1)
-	next_page = page + 1
-	prev_page = page - 1
+	data = {}
+	url = "https://2wf0rg.deta.dev/api/cards/?skip="+str(page*9)+"&top=1"
+	data["has_next"] = has_next(url=url)
+	data["next_page"] = "/page/"+str(page + 1)
+	data["prev_page"] = "/page/"+str(page - 1)
+	if page > 1:
+		data["has_prev"] = True
+	else:
+		data["has_prev"] = False
 
-	return render_template('home.html', categories=categories, cards=cards, has_next=has_next, has_prev=True, next_page=next_page, prev_page=prev_page)
+	return render_template('home.html', categories=categories, cards=cards, data=data)
 
-@app.route('/category/<string:category>')
-def category(category):
-	return 'Category: ' + category
+
+@app.route('/category/<string:category>/page/<int:page>')
+def category(category, page):
+	category = category.replace(" ", "%20")
+	skip = (page - 1) * 9
+	categories = get_categories()
+	cards = get_cards(skip=skip, category=category)
+	data = {}
+	url = "https://2wf0rg.deta.dev/api/cards/?category="+category+"&skip="+str(page*9)+"&top=1"
+	data["has_next"] = has_next(url=url)
+	data["next_page"] = "/category/"+category+"/page/"+str(page + 1)
+	data["prev_page"] = "/category/"+category+"/page/"+str(page - 1)
+	if page > 1:
+		data["has_prev"] = True
+	else:
+		data["has_prev"] = False
+
+	return render_template('home.html', categories=categories, cards=cards, data=data)
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
